bfa35f9104d8b4ab4e8a14e9dc3ffb9af47d8565
shreehari-n <146311318+Shree0410@users.noreply.github.com>
2026-08-10 21:19:07 +0530

    Update inventory_page.ts

--- pages/inventory_page.ts
+++ pages/inventory_page.ts
@@ -1,17 +1,9 @@
 import { Page } from "@playwright/test";
-export class InventoryPage{
+export class loginPage{
     page: Page;
     constructor(page: Page){
         this.page=page;
     }
 
-async login(username: string, password: string){
-    await this.page.fill('#user-name', username);
-    await this.page.fill('#password', password);
-    await this.page.click('#login-button');
-}
-async logout(){
-    await this.page.locator('#react-burger-menu-btn').click();
-    await this.page.locator('#logout_sidebar_link').click();
-}
+
 }
